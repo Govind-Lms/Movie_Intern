0a1ba58c31f0ea2323f2f7e65b07c97bcbd1e739
Govind Lamsal <govindlms@Govinds-MacBook-Pro.local>
2024-06-11 14:19:21 +0630

    hive

--- lib/views/all_screen.dart/bottom_nav.dart
+++ lib/views/all_screen.dart/bottom_nav.dart
@@ -1,7 +1,6 @@
 
 import 'package:animated_bottom_navigation_bar/animated_bottom_navigation_bar.dart';
 import 'package:flutter/material.dart';
-import 'package:hive_flutter/hive_flutter.dart';
 import 'package:movie_intern/views/all_screen.dart/popular_screen.dart';
 import 'package:movie_intern/views/favorites/fav_screen.dart';
 import 'package:movie_intern/views/genres/genre_screen.dart';
